2f72a1146520044c56758e781f71e724b66b3d5d
daeun <daa3230@gmail.com>
2020-11-22 22:54:39 +0900

    add with custome hook

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -23,35 +24,60 @@ const NewsListBlock = styled.div`
 //   urlToImage: 'https://via.placeholder.com/106',
 // };
 
-const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
+// const NewsList = ({ category }) => {
+//   const [articles, setArticles] = useState(null);
+//   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    //declare the function which use aync separately
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=fed53298bc39493291bd5555dc9ab3b6`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+//   useEffect(() => {
+//     //declare the function which use aync separately
+//     const fetchData = async () => {
+//       setLoading(true);
+//       try {
+//         const query = category === 'all' ? '' : `&category=${category}`;
+//         const response = await axios.get(
+//           `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=fed53298bc39493291bd5555dc9ab3b6`,
+//         );
+//         setArticles(response.data.articles);
+//       } catch (e) {
+//         console.log(e);
+//       }
+//       setLoading(false);
+//     };
+//     fetchData();
+//   }, [category]);
+
+//   if (loading) {
+//     return <NewsListBlock>Loading ... </NewsListBlock>;
+//   }
+//   if (!articles) {
+//     return null;
+//   }
+
+//   return (
+//     <NewsListBlock>
+//       {articles.map((article) => (
+//         <NewsItem key={article.url} article={article} />
+//       ))}
+//     </NewsListBlock>
+//   );
+// };
+
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=fed53298bc39493291bd5555dc9ab3b6`,
+    );
   }, [category]);
 
   if (loading) {
     return <NewsListBlock>Loading ... </NewsListBlock>;
   }
-  if (!articles) {
-    return null;
+  if (error) {
+    return <NewsListBlock>Random Error Occured!</NewsListBlock>;
   }
 
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
